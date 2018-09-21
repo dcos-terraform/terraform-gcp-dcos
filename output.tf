5d563fb92ac293e04aa4ac89b348b3fe72358fad
Jan Ulferts <julferts@mesosphere.com>
2018-09-21 15:58:09 +0200

    added master ips and public agent load balancer

--- output.tf
+++ output.tf
@@ -1,4 +1,14 @@
+output "masters-ips" {
+  description = "Master IP addresses"
+  value       = "${coalescelist(module.dcos-infrastructure.masters.public_ips, module.dcos-infrastructure.masters.private_ips)}"
+}
+
 output "masters-loadbalancer" {
   description = "This is the load balancer address to access the DC/OS UI"
   value       = "${module.dcos-infrastructure.forwarding_rules.masters}"
+}
+
+output "public-agents-loadbalancer" {
+  description = "This is the load balancer address to access the DC/OS public agents"
+  value       = "${module.dcos-infrastructure.forwarding_rules.public_agents}"
 }
