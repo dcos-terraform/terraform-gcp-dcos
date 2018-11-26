51294cdc577fff61d4fbf039554d50d36be388e4
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-11-26 11:04:10 -0800

    docs: adding version support to convenience wrapper

--- docs/published/main.tf
+++ docs/published/main.tf
@@ -8,7 +8,8 @@ data "http" "whatismyip" {
 }
 
 module "dcos" {
-  source = "dcos-terraform/dcos/gcp"
+  source  = "dcos-terraform/dcos/gcp"
+  version = "~> 0.1"
 
   dcos_instance_os    = "coreos_1855.5.0"
   cluster_name        = "my-open-dcos-cluster"
