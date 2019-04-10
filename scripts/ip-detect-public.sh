c81abd7af1f5e8272dd92e63d401e26aaeaa7f43
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-04-10 14:42:12 +0200

    fix: bring back scripts

---
+++ scripts/ip-detect-public.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Example ip-detect script using an external authority
+# Uses the GCP Metadata Service to get the node's internal
+# ipv4 address
+set -o nounset -o errexit
+
+curl -fsSL http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H 'Metadata-Flavor: Google'
