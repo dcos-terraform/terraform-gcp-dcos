8fbfbc005f35d50ab4a9d72f868a82ddb916cb1d
Jan Ulferts <julferts@mesosphere.com>
2020-06-04 14:08:18 +0200

    fix: no dots allowed in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,17 @@
 output "masters-ips" {
   description = "Master IP addresses"
   value = coalescelist(
-    module.dcos-infrastructure.masters.public_ips,
-    module.dcos-infrastructure.masters.private_ips,
+    module.dcos-infrastructure.masters_public_ips,
+    module.dcos-infrastructure.masters_private_ips,
   )
 }
 
 output "masters-loadbalancer" {
   description = "This is the load balancer address to access the DC/OS UI"
-  value       = module.dcos-infrastructure.forwarding_rules.masters
+  value       = module.dcos-infrastructure.forwarding_rules_masters
 }
 
 output "public-agents-loadbalancer" {
   description = "This is the load balancer address to access the DC/OS public agents"
-  value       = module.dcos-infrastructure.forwarding_rules.public_agents
+  value       = module.dcos-infrastructure.forwarding_rules_public_agents
 }
-
